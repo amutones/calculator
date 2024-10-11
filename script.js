bc0caacf18c025b6eda75a9fb17e0178cfd26685
amutones <aurgena@gmail.com>
2024-10-11 09:43:12 -0500

    Added button listeners

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+let firstNum = 0;
+let secondNum = 0;
+let operator = "";
+let numButtons = document.querySelector("#numbers");
+let functionButtons = document.querySelector("#functions");
+let display = document.querySelector("#display");
+
+numButtons.addEventListener("click", function() {
+    display.textContent = "hi";
+})
+
+functionButtons.addEventListener("click", function() {
+    alert("hi");
+})
+
+function operate(a, b, c) {
+    switch (c) {
+        case "+":
+            add(a,b);
+            break;
+        case "-":
+            subtract(a,b);
+            break;
+        case "*":
+            multiply(a,b);
+            break;
+        case "/":
+            divide(a,b);
+            break;   
+        default:
+            break;
+    }
+}
 
 function add(a, b){
     return a + b;
@@ -10,4 +43,7 @@ function multiply(a, b) {
 }
 function divide(a, b) {
     return a / b;
+}
+function clear() {
+
 }
